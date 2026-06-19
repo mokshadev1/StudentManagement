54578043c11cf2590df75391d4b05f3a9b7fa8e4
Moksha <mokshadev1306@gmail.com>
2026-06-19 09:04:33 +0530

    Edit Login Function

--- main.java
+++ main.java
@@ -1,5 +1,24 @@
+import java .util.Scanner;
 public class main {
     public static void main(String[] args){
         System.out.println("Student Management System");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter User Name :");
+        String userName = sc.nextLine();
+        System.out.println("Enter Password :");
+        String password = sc.nextLine();
+        String user1 = "Ram";
+        String p1 = "1234";
+        String user2 = "Deepanshu321";
+        String p2 = "dl@321";
+
+        if(userName.equals(user1)&& password.equals(p1)){
+            System.out.println("Welcome " + userName +" Login Successful");}
+            else if (userName.equals(user2) && password.equals(p2)){
+                System.out.println("Welcome " + user2 +" Login Successful");}
+            else {
+                System.out.println("Invalid Details Password or Username Incorrect ");
+        } 
+
     }
 }
